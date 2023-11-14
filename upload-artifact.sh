09ee0544cbf85152206b61af23d3d82e3a780088
Reinout van Rees <reinout@vanrees.org>
2023-11-14 09:47:02 +0100

    Adjusted script to this repo

--- upload-artifact.sh
+++ upload-artifact.sh
@@ -2,11 +2,11 @@
 set -e
 set -u
 
-VERSION=$(grep "^version" ./threedi_models_and_simulations/metadata.txt | cut -d= -f2)
+VERSION=$(grep "^version" ./lizard_qgis_plugin/metadata.txt | cut -d= -f2)
 
-# ARTIFACTS_KEY should be set as env variable in the travis UI.
-# TRAVIS_BRANCH is set automatically by travis
-ARTIFACT=threedi_models_and_simulations.${VERSION}.zip
+# ARTIFACTS_KEY is set as env variable in the github action.
+# For now you have to set it manually locally.
+ARTIFACT=lizard_qgis_plugin.${VERSION}.zip
 PROJECT=threedi-api-qgis-client
 
 curl -X POST \
